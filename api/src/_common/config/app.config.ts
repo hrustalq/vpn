4719695850b4e0edf4f86601eb3d43290dc03b06
hrustalq <n1k3f1t@gmail.com>
2024-08-24 19:17:39 +0800

    add global config

---
+++ api/src/_common/config/app.config.ts
@@ -0,0 +1,4 @@
+export default {
+  port: process.env.APP_PORT,
+  origin: process.env.APP_ORIGIN,
+};
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { appConfig } from './_common/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({ origin: appConfig.origin });
+  await app.listen(appConfig.port);
 }
 bootstrap();
