2ccbbc87cb59190f7ab899c9ca7c8f2612e0b26a
hrustalq <n1k3f1t@gmail.com>
2024-08-24 19:43:47 +0800

    add auth provider to entrypoint

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,12 +2,14 @@ import React, { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import { RouterProvider } from "./shared/providers";
+import { RouterProvider, AuthProvider } from "./shared/providers";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider />
+    <AuthProvider>
+      <RouterProvider />
+    </AuthProvider>
   </StrictMode>,
 );
